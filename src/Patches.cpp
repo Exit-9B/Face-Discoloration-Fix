19793d749cddfe0f4e93451fb11a9bb50676b604
Parapets <southwest85b@gmail.com>
2020-11-19 07:51:12 +0000

    Fix for crashes loading bad tint data

--- src/Patches.cpp
+++ src/Patches.cpp
@@ -6,6 +6,37 @@ inline static std::array<uint8_t, 6> nop6{ 0x66, 0x0F, 0x1F, 0x44, 0x00, 0x00 };
 inline static std::array<uint8_t, 1> jcc2_to_jmp{ 0xEB };
 inline static std::array<uint8_t, 2> jcc6_to_jmp{ 0x90, 0xE9 };
 
+RE::TESRace::FaceRelatedData::TintAsset* FindTintAssetData(RE::TESNPC* a_actor, std::uint16_t a_index)
+{
+	auto race = a_actor->race;
+	if (!race)
+	{
+		logger::warn("{} [NPC_:{:x}] has face tint data but no race.", a_actor->fullName, a_actor->formID);
+		return nullptr;
+	}
+
+	for (int i = 0; i < RE::SEXES::kTotal; i++)
+	{
+		auto faceData = race->faceRelatedData[i];
+		if (faceData && faceData->tintMasks)
+		{
+			auto tintMasks = *faceData->tintMasks;
+			for (RE::BSTArrayBase::size_type j = 0; j < tintMasks.size(); j++)
+			{
+				auto tintAsset = tintMasks[j];
+				if (tintMasks[j]->texture.index == a_index)
+				{
+					return tintAsset;
+				}
+			}
+		}
+	}
+
+	logger::warn("{} [NPC_:{:x}] has invalid face tint data.", a_actor->fullName, a_actor->formID);
+
+	return nullptr;
+}
+
 void ApplyFaceDiscolorationFix()
 {
 	// Load tint layers for all NPCs
@@ -26,6 +57,14 @@ void ApplyFaceDiscolorationFix()
 	// Fix dark face
 	static REL::Relocation<std::uintptr_t> hook_tint{ Offset::BSFaceGenDB_GenerateHeadPartModel, 0x3A4 };
 	REL::safe_write(hook_tint.address(), nop2);
+
+	constexpr REL::ID TESNPC_InitializeAfterAllFormsAreReadFromFile{ 24215 };
+	static REL::Relocation<std::uintptr_t> hook_findtintasset{ TESNPC_InitializeAfterAllFormsAreReadFromFile, 0x766 };
+
+	auto& trampoline = SKSE::GetTrampoline();
+	std::array<std::uint8_t, 6> mov_rcx_r15{ 0x4C, 0x89, 0xF9, 0x0F, 0x1F, 0x00 };
+	REL::safe_write(hook_findtintasset.address(), mov_rcx_r15);
+	trampoline.write_call<6>(hook_findtintasset.address() + 6, FindTintAssetData);
 
 	logger::info("Applied patches for face discoloration fix");
 }
